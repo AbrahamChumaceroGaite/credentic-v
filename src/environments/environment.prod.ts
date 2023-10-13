10eea323ac2870870c0f58fe9ec102eb7c151daa
AbrahamChumaceroGaite <abraham.chumacero@ucb.edu.bo>
2023-10-13 10:04:28 -0300

    development

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,18 @@
 export const environment = {
     production: true,
-/*     api: 'https://cert-api.tja.ucb.edu.bo/credentic/api',
+    api: 'https://cert-api.tja.ucb.edu.bo/credentic/api',
     apimg: 'https://cert-api.tja.ucb.edu.bo/',
     apic: 'https://cert.tja.ucb.edu.bo/certificado/codigo/',
     apiurl: 'https://cert.tja.ucb.edu.bo/certificado/chk/codigo/',
-    apiqr: 'https://cert.tja.ucb.edu.bo/certificado/qr/codigo/' */
+    apiqr: 'https://cert.tja.ucb.edu.bo/certificado/qr/codigo/',
+    apid: 'https://cert.tja.ucb.edu.bo/certificado/download/codigo/'
 
       //API DEVEPLONMENT	
-  api: 'https://credentic-api.dev.404.codes/credentic/api',
+/*   api: 'https://credentic-api.dev.404.codes/credentic/api',
   apimg: 'https://credentic-api.dev.404.codes/',
   apic: 'https://credentic-api.dev.404.codes/certificado/codigo/',
   apiurl: 'https://credentic-api.dev.404.codes/certificado/chk/codigo/',
   apiqr: 'https://credentic-api.dev.404.codes/certificado/qr/codigo/',
-  apid: 'https://credentic-api.dev.404.codes/certificado/download/codigo/'
+  apid: 'https://credentic-api.dev.404.codes/certificado/download/codigo/' */
 };
 
